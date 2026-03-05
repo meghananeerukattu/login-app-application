1edb57222c13b9c6898652b4f51f1bc8834a3778
Neerukattu-Meghana <nmeghana293@gmail.com>
2026-03-05 06:43:22 +0530

    Updated backend server code

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require("express")
+const cors = require("cors")
 const app = express()
-
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
+const PORT = process.env.PORT || 5000
 
-const PORT = 5000
-app.post('/login', (req, res) => {
-
-    const { username, password } = req.body
-    if(username === "admin" && password === "admin"){
-        res.status(200).json({ message: "Login successful" })
-    } 
-    else{
-        res.status(401).json({ message: "Invalid credentials" })
-    }
-
+// Root route (to check server is running)
+app.get("/", (req, res) => {
+    res.send("Login API is running")
 })
+
+// Login API
+app.post("/login", (req, res) => {
+    const { username, password } = req.body
+    if (username === "admin" && password === "admin") {
+        res.status(200).json({
+            message: "Login successful"
+        })
+    } else {
+        res.status(401).json({
+            message: "Invalid credentials"
+        })
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
