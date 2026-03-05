17d10ee2e7f7f3cc2f428a60dcf344b77176882b
Neerukattu-Meghana <nmeghana293@gmail.com>
2026-03-05 06:45:46 +0530

    Adding root route

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,25 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-app.use(cors())
-app.use(express.json())
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
 
-// Root route (to check server is running)
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
-    res.send("Login API is running")
-})
+  res.send("Login API is running");
+});
 
-// Login API
 app.post("/login", (req, res) => {
-    const { username, password } = req.body
-    if (username === "admin" && password === "admin") {
-        res.status(200).json({
-            message: "Login successful"
-        })
-    } else {
-        res.status(401).json({
-            message: "Invalid credentials"
-        })
-    }
-})
+  const { username, password } = req.body;
 
+  if (username === "admin" && password === "admin") {
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
